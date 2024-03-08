067e23ba96d15e0bfe6518dd2ae9275d1186d9a7
anttmin <anttminm@gamil.com>
2024-03-08 19:30:27 +0630

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,9 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Main from "./layouts/Main";
-import Posts from "./pages/Posts";
-import Create from "./pages/Create";
-import { loader as PostLoder } from "./pages/Posts";
-import Details from "./pages/Details";
-import { loader as DetailLoader } from "./pages/Details";
-import { action as PostcreateAction } from "./pages/Create";
+import React from 'react'
 
 const App = () => {
-  const router = createBrowserRouter([
-  { 
-    path: "/", 
-    element: <Main />,
-    children:[
-    {
-      index:true,
-      element : <Posts />,
-      loader : PostLoder
-    },
-    {
-      path:"/create-post",
-      element : <Create />,
-      action : PostcreateAction
-    },
-    {
-      path:"/post-details/:id",
-      element: <Details />,
-      loader : DetailLoader 
-    }
-  ]
-
-  }]);
   return (
-    <RouterProvider router={router}/>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
